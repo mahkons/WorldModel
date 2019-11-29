c5c015396dcb2c2a28d83720dfcf674ec5ea7661
maybe-hello-world <maybe.hello.world@gmail.com>
2019-11-29 16:30:55 +0300

    VAE added

---
+++ worldmodels/VAE/VAE.py
@@ -0,0 +1,99 @@
+import typing
+
+import torch
+import torch.nn as nn
+from torch.nn import functional as f
+from torchvision.transforms.functional import to_tensor
+
+
+class VAE(nn.Module):
+    # PyCharm problems
+    # https://youtrack.jetbrains.com/issue/PY-37601
+    def __call__(self, *inp, **kwargs) -> typing.Any:
+        return super().__call__(*inp, **kwargs)
+
+    def __init__(
+            self,
+            image_height: int = 56,
+            image_width: int = 64,
+            image_channels: int = 3,
+            z_dim: int = 32,
+            device: str = "cpu"
+    ):
+        super().__init__()
+        self.device = device
+        self.h = image_height
+        self.w = image_width
+        self.c = image_channels
+        self.z_dim = z_dim
+
+        self.en_cnn1 = nn.Conv2d(in_channels=self.c, out_channels=32, kernel_size=3, stride=2)
+        self.en_cnn2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=2, stride=2)
+        self.en_cnn3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=2, stride=1)
+
+        self.en_dense_mu1 = nn.Linear(in_features=21504, out_features=self.z_dim)
+        self.en_dense_s1 = nn.Linear(in_features=21504, out_features=self.z_dim)
+
+        self.de_dense1 = nn.Linear(in_features=self.z_dim, out_features=12544)
+        self.de_deconv1 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=(2, 4), stride=2)
+        self.de_deconv2 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=2, stride=2)
+        self.de_deconv3 = nn.ConvTranspose2d(in_channels=64, out_channels=self.c, kernel_size=2, stride=2)
+
+        self.to(self.device)
+
+    def encode(self, x: torch.Tensor):
+        x = x.to(self.device)
+        x = f.relu(self.en_cnn1(x))
+        x = f.relu(self.en_cnn2(x))
+        x = f.relu(self.en_cnn3(x))
+        x = x.view(x.size(0), -1)
+
+        mu = self.en_dense_mu1(x)
+        logstd = self.en_dense_s1(x)
+
+        return mu, logstd
+
+    def decode(self, x: torch.Tensor):
+        x = x.to(self.device)
+        x = f.relu(self.de_dense1(x))
+        x = x.view(-1, 256, 7, 7)
+        x = f.relu(self.de_deconv1(x))
+        x = f.relu(self.de_deconv2(x))
+        x = torch.sigmoid(self.de_deconv3(x))
+        return x
+
+    def reparameterize(self, mu: torch.Tensor, logstd: torch.Tensor):
+        if self.training:
+            std = (logstd * 0.5).exp_()
+            std_prob = torch.randn(*mu.size())
+            return mu + std_prob * std
+        else:
+            return mu   # inference time
+
+    def forward(self, x: torch.Tensor):
+        x = x.to(self.device)
+        mu, logstd = self.encode(x)
+        z = self.reparameterize(mu, logstd)
+        z = self.decode(z)
+        return z, mu, logstd
+
+    @staticmethod
+    def calculate_loss(pred_x: torch.Tensor, true_x: torch.Tensor, mu: torch.Tensor, logstd: torch.Tensor):
+        bce = f.mse_loss(pred_x, true_x, size_average=False)
+        kld = -0.5 * torch.sum(1 + logstd - mu.pow(2) - logstd.exp())
+        return bce + kld
+
+    def play_encode(self, obs: torch.Tensor) -> torch.Tensor:
+        with torch.no_grad():
+            obs = to_tensor(obs).unsqueeze(0).float()
+            return self.reparameterize(*self.encode(obs)).squeeze()
+
+    def save_model(self, path):
+        torch.save(self.state_dict(), path)
+
+    @classmethod
+    def load_model(cls, path, *args, **kwargs):
+        state_dict = torch.load(path)
+        vae = cls(*args, **kwargs)
+        vae.load_state_dict(state_dict=state_dict)
+        return vae
