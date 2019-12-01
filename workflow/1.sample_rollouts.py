c9860ea49fe968ec57141f3856e5072054178ff2
Makhnev Konstantin <mah.kons@gmail.com>
2019-12-02 01:24:43 +0300

    simple rollouts generation added

---
+++ workflow/1.sample_rollouts.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import gym
+import imageio
+import numpy as np
+from tqdm import tqdm
+from random import choice, random, randint
+import argparse
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--episodes', type=int, default=1, required=False)
+    parser.add_argument('--show', type=lambda x: (str(x).lower() in ['true','1', 'yes']), default=False, required=False)
+    parser.add_argument('--steps', type=int, default=10**10, required=False)
+    return parser 
+
+
+def get_action(env):
+    return env.action_space.sample()
+
+if __name__ == "__main__":
+    env = gym.make("CarRacing-v0")
+    args = create_parser().parse_args()
+
+    pbar = tqdm(range(args.episodes))
+    for episode in pbar:
+        pbar.set_description("Episode [{}/{}]".format(episode + 1, args.episodes))
+
+        obs = env.reset()
+        for t in range(args.steps):
+            action = get_action(env)
+            obs, reward, done, _ = env.step(action)
+            if args.show:
+                env.render()
+            if done:
+                break
+
+            i = ('000' + str(t))[-3:]
+            imageio.imwrite(f'./rollouts/SimplePolicy/car_{episode}_{i}.jpg', obs)
