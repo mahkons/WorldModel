eb8be768a0886c7652a3b44eaf43465c6f3dd9e1
Makhnev Konstantin <mah.kons@gmail.com>
2019-12-02 13:07:54 +0300

    add vae testing

---
+++ workflow/3.test_vae.py
@@ -0,0 +1,33 @@
+import sys
+import os
+# path hack
+sys.path.insert(0, os.path.abspath('..'))
+
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import torch
+from torchvision import transforms
+from torchvision import datasets
+from torchvision.utils import make_grid
+
+from worldmodels.VAE.VAE import VAE
+
+def show(img):
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1,2,0)), interpolation='nearest')
+
+def compare(model, dataset):
+    with torch.no_grad():
+        x = dataset[random.randint(1, 100)][0].unsqueeze(0)
+        recon_x, _, _ = model(x)
+        show(make_grid(torch.cat([x, recon_x])))
+        plt.show()
+
+if __name__ == "__main__":
+    dataset = datasets.ImageFolder(root='rollouts', transform=transforms.Compose([
+        transforms.Resize(64),
+        transforms.ToTensor(), 
+    ]))
+    vae = VAE.load_model("generated/vae.torch")
+    compare(vae, dataset)
