8f097316a56c9a7cd0ba8de6d1131acdf6dcdcb7
Makhnev Konstantin <mah.kons@gmail.com>
2020-01-10 14:05:15 +0300

    add sampling with agent for iterative version

--- workflow/1.sample_rollouts.py
+++ workflow/1.sample_rollouts.py
@@ -8,6 +8,8 @@ from random import choice, random, randint
 import argparse
 import torch
 
+from params import z_size, n_hidden, n_gaussians, image_height, image_width, actor_lr, critic_lr, mem_size
+
 def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--env', type=str, default='LunarLanderContinuous-v2')
@@ -19,7 +21,7 @@ def create_parser():
 
 
 # TODO decide how to choose action
-def get_action(env, steps):
+def get_action_randomly(env, steps, obs):
     action = env.action_space.sample()
     #  return action
 
@@ -30,7 +32,28 @@ def get_action(env, steps):
     return action
 
 
-def sample_rollouts(env, epochs, steps, images):
+class GetActionWithAgent:
+    def __init__(self):
+        device = torch.device('cpu')
+        self.controller = ControllerAC.load_model(
+                "generated/actor_critic.torch", 
+                state_sz,
+                action_sz,
+                n_hidden,
+                memory=memory,
+                device=device,
+                actor_lr=actor_lr,
+                critic_lr=critic_lr
+            )
+
+        self.vae = VAE.load_model('generated/vae.torch', image_channels=3, image_height=image_height, image_width=image_width)
+
+    def __call__(self, env, steps, obs):
+        state = self.vae.play_encode(obs)
+        action = self.controller.select_action(state)
+
+
+def sample_rollouts(env, epochs, steps, images, get_action):
     if images:
         os.makedirs('rollouts/SimplePolicy', exist_ok=True) 
         os.system('rm -rf rollouts/SimplePolicy/*')
@@ -44,7 +67,7 @@ def sample_rollouts(env, epochs, steps, images):
 
         obs = env.reset()
         for t in range(steps):
-            action = get_action(env, t)
+            action = get_action(env, t, obs)
             obs, reward, done, _ = env.step(action)
             if args.show:
                 env.render()
@@ -75,5 +98,10 @@ if __name__ == '__main__':
     else:
         raise ValueError('Unknown env')
 
-    sample_rollouts(env, args.epochs, args.steps, images)
+    if args.with_agent:
+        get_action = GetActionWithAgent
+    else:
+        get_action = get_action_randomly
+
+    sample_rollouts(env, args.epochs, args.steps, images, get_action)
     env.close()
